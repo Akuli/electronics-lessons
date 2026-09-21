eedc5ffd41a32bacd00aa8247ad8945e6dad45f2
Akuli <akuviljanen17@gmail.com>
2026-09-21 19:02:14 +0300

    Add cheat sheet page

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 set -ex -o pipefail
 
-python3 txt2html.py < index.txt > index.html
+python3 txt2html.py --no-nav < index.txt > index.html
+python3 txt2html.py < cheat-sheet.txt > cheat-sheet.html
+
 for index_txt in ??/index.txt; do
     (cd $(dirname $index_txt) && python3 ../txt2html.py < index.txt > index.html)
 done
